46c50dc6fe0f22399376c18f35050e5a2136e2bc
phillcunnington <phillcunnington@ca16bcf9-9eb2-46e4-97b8-9b07c30c95dc>
2013-05-13 18:49:36 +0000

    Falling back to ad-passthrough for anonymous user and if the X-OpenIDM-username is set on the request (i.e. IWA is not available)

--- forgerock-authn-filter/trunk/forgerock-jaspic/forgerock-jaspi-modules/forgerock-jaspi-iwa-module/src/main/java/org/forgerock/jaspi/modules/IWAModule.java
+++ forgerock-authn-filter/trunk/forgerock-jaspic/forgerock-jaspi-modules/forgerock-jaspi-iwa-module/src/main/java/org/forgerock/jaspi/modules/IWAModule.java
@@ -2,7 +2,7 @@ package org.forgerock.jaspi.modules;
 
 import org.forgerock.jaspi.modules.wdsso.Base64;
 import org.forgerock.jaspi.modules.wdsso.WDSSO;
-import org.forgerock.openidm.filter.UserWrapper;
+import org.forgerock.openidm.jaspi.module.adpassthrough.ADPassthroughModule;
 
 import javax.security.auth.Subject;
 import javax.security.auth.callback.CallbackHandler;
@@ -13,6 +13,7 @@ import javax.security.auth.message.MessagePolicy;
 import javax.security.auth.message.module.ServerAuthModule;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.Map;
@@ -24,11 +25,15 @@ public class IWAModule implements ServerAuthModule {
     private CallbackHandler handler;
     private Map options;
 
+    private ADPassthroughModule adPassthroughModule;
+
     @Override
     public void initialize(MessagePolicy requestPolicy, MessagePolicy responsePolicy, CallbackHandler handler,
             Map options) throws AuthException {
         this.handler = handler;
         this.options = options;
+        this.adPassthroughModule = new ADPassthroughModule();
+        adPassthroughModule.initialize(requestPolicy, responsePolicy, handler, options);
     }
 
     @Override
@@ -40,9 +45,23 @@ public class IWAModule implements ServerAuthModule {
     public AuthStatus validateRequest(MessageInfo messageInfo, Subject clientSubject, Subject serviceSubject)
             throws AuthException {
 
-
         HttpServletRequest request = (HttpServletRequest)messageInfo.getRequestMessage();
         HttpServletResponse response = (HttpServletResponse)messageInfo.getResponseMessage();
+
+        String xOpenIDMUsername = request.getHeader("X-OpenIDM-username");
+        String xOpenIdmPassword = request.getHeader("X-OpenIDM-password");
+        HttpSession session = request.getSession(false);
+        boolean haveOpenIDMUsername = false;
+        if (xOpenIDMUsername != null && xOpenIdmPassword != null) {
+            haveOpenIDMUsername = xOpenIDMUsername != null;
+        }
+        if (!haveOpenIDMUsername && session != null) {
+            haveOpenIDMUsername = session.getAttribute("openidm.username") != null;
+        }
+        if (haveOpenIDMUsername) {
+            //skip straight to ad passthrough
+            return adPassthroughModule.validateRequest(messageInfo, clientSubject, serviceSubject);
+        }
 
         String httpAuthorization = request.getHeader("Authorization");
 
@@ -56,7 +75,7 @@ public class IWAModule implements ServerAuthModule {
             response.addHeader("WWW-Authenticate", "Negotiate");
             response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
             try {
-                response.getWriter().write("{\"failure\":true,\"reason\":\"" + IWA_FAILED + "\", \"egtoken\":\"" + Base64.encode(new byte[]{0x60}/*"THISISATOKEN".getBytes("UTF-8")*/) + "\"}");
+                response.getWriter().write("{\"failure\":true,\"reason\":\"" + IWA_FAILED + "\"}");
                 response.getWriter().flush();
                 response.getWriter().close();
             } catch (IOException e) {
@@ -73,7 +92,8 @@ public class IWAModule implements ServerAuthModule {
             try {
                 iwaUserName = new WDSSO().process(options, request);
             } catch (RuntimeException e) {
-                return AuthStatus.FAILURE;
+                //TODO fallback to AD passthrough
+                return adPassthroughModule.validateRequest(messageInfo, clientSubject, serviceSubject);
             }
 
             final String USERNAME_ATTRIBUTE = "openidm.username";
@@ -82,15 +102,20 @@ public class IWAModule implements ServerAuthModule {
             final String RESOURCE_ATTRIBUTE = "openidm.resource";
             final String OPENIDM_AUTHINVOKED = "openidm.authinvoked";
 
-            String username = request.getHeader("X-OpenIDM-username") == null ? iwaUserName.substring(0, iwaUserName.indexOf("@")) : request.getHeader("X-OpenIDM-username");
-            String password = request.getHeader("X-OpenIDM-password");
+            String username = null;
+//            String xOpenIDMUsername = request.getHeader("X-OpenIDM-username");
+//            if (xOpenIDMUsername != null && "anonymous".equals(xOpenIDMUsername)) {
+//                username = xOpenIDMUsername;
+//            } else {
+                username = iwaUserName.substring(0, iwaUserName.indexOf("@"));
+//            }
 
             request.setAttribute(USERNAME_ATTRIBUTE, username);
-            request.setAttribute(USERID_ATTRIBUTE, options.get("userid"));//"%3CGUID%3D2d9010802060b040a6df6aaba3c9bdc3%3E");
-            request.setAttribute(ROLES_ATTRIBUTE, Arrays.asList(new String[]{"openidm-reg"}));
+//            request.setAttribute(USERID_ATTRIBUTE, options.get("userid"));//"%3CGUID%3D2d9010802060b040a6df6aaba3c9bdc3%3E");
+//            request.setAttribute(ROLES_ATTRIBUTE, Arrays.asList(new String[]{"openidm-reg"}));
             request.setAttribute(RESOURCE_ATTRIBUTE, "system/AD/account");
             request.setAttribute(OPENIDM_AUTHINVOKED, "authnfilter");
-            messageInfo.setRequestMessage(new UserWrapper(request, username, "%3CGUID%3D2d9010802060b040a6df6aaba3c9bdc3%3E", Arrays.asList(new String[]{"openidm-reg"})));
+//            messageInfo.setRequestMessage(new UserWrapper(request, username, null, Arrays.asList(new String[]{"openidm-reg"})));
             return AuthStatus.SUCCESS;
         }
     }
