9ec6cf664e1bf054ec5a7160eaf26b7fed852091
jake.feasel <jake.feasel@ca16bcf9-9eb2-46e4-97b8-9b07c30c95dc>
2014-03-17 22:57:26 +0000

    [maven-release-plugin]  copy for branch 1.0.10

---
+++ forgerock-ui/branches/1.0.10/forgerock-ui-commons/src/main/js/org/forgerock/commons/ui/common/SiteConfigurator.js
@@ -0,0 +1,131 @@
+/**
+* DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+*
+* Copyright (c) 2011-2012 ForgeRock AS. All rights reserved.
+*
+* The contents of this file are subject to the terms
+* of the Common Development and Distribution License
+* (the License). You may not use this file except in
+* compliance with the License.
+*
+* You can obtain a copy of the License at
+* http://forgerock.org/license/CDDLv1.0.html
+* See the License for the specific language governing
+* permission and limitations under the License.
+*
+* When distributing Covered Code, include this CDDL
+* Header Notice in each file and include the License file
+* at http://forgerock.org/license/CDDLv1.0.html
+* If applicable, add the following below the CDDL Header,
+* with the fields enclosed by brackets [] replaced by
+* your own identifying information:
+* "Portions Copyrighted [year] [name of copyright owner]"
+*/
+
+/*global $, define, require */
+
+/**
+* @author mbilski
+*/
+define("org/forgerock/commons/ui/common/SiteConfigurator", [
+   "org/forgerock/commons/ui/common/main/AbstractConfigurationAware",
+   "org/forgerock/commons/ui/common/util/Constants", 
+   "org/forgerock/commons/ui/common/main/EventManager",
+   "org/forgerock/commons/ui/common/main/Configuration",
+   "org/forgerock/commons/ui/common/main/i18nManager"
+], function(AbstractConfigurationAware, constants, eventManager, conf, i18nManager) {
+   var obj = new AbstractConfigurationAware();
+   
+   obj.initialized = false;
+   
+   $(document).on(constants.EVENT_READ_CONFIGURATION_REQUEST, function() {
+       var configurationDelegate;
+
+       if(!conf.globalData) {
+           conf.setProperty('globalData', {});
+           conf.globalData.auth = {};
+       }
+
+       if(!conf.delegateCache) {
+           conf.setProperty('delegateCache', {});
+       }
+
+       console.log("READING CONFIGURATION");
+       if(obj.configuration && obj.initialized === false) {
+           obj.initialized = true;
+           
+           if(obj.configuration.remoteConfig === true) {
+               configurationDelegate = require(obj.configuration.delegate);
+               configurationDelegate.getConfiguration(function(config) {
+                   obj.processConfiguration(config); 
+                   eventManager.sendEvent(constants.EVENT_APP_INTIALIZED);
+               }, function() {
+                   obj.processConfiguration({}); 
+                   eventManager.sendEvent(constants.EVENT_APP_INTIALIZED);                   
+               });
+           } else {
+               obj.processConfiguration(obj.configuration); 
+               eventManager.sendEvent(constants.EVENT_APP_INTIALIZED);
+           }          
+       }
+   });
+   
+   obj.processConfiguration = function(config) {
+       var router, changeSecurityDataDialog;
+       
+       router = require("org/forgerock/commons/ui/common/main/Router");
+       
+       if(config.selfRegistration === true) {
+           conf.globalData.selfRegistration = true;
+       } else {               
+           if(router.configuration && router.configuration.routes.register) {
+               console.log("Removing register route.");
+               delete router.configuration.routes.register;
+           }
+       }
+       
+       if(config.securityQuestions === true) {
+           conf.globalData.securityQuestions = true;             
+       } else {
+           if(router.configuration && router.configuration.routes.forgottenPassword) {
+               console.log("Removing forgottenPassword route.");
+               delete router.configuration.routes.forgottenPassword;
+           } 
+       }
+       
+       if(config.siteIdentification === true) {
+           conf.globalData.siteIdentification = true;
+       } else {
+           if(router.configuration && router.configuration.routes.siteIdentification) {
+               console.log("Removing siteIdentification route.");
+               delete router.configuration.routes.siteIdentification;
+           }
+       }
+       
+       if(config.siteImages) {
+           conf.globalData.siteImages = config.siteImages;
+       }
+       
+       if (config.passwordResetLink) {
+           conf.globalData.passwordResetLink = config.passwordResetLink;
+       }
+     
+       i18nManager.init();
+      
+       if(config.roles) {
+           conf.globalData.userRoles = config.roles;
+       }
+       
+       if(config.notificationTypes) {
+           conf.notificationTypes = config.notificationTypes;
+       }
+       
+       if(config.defaultNotificationType) {
+           conf.defaultType = config.defaultNotificationType;
+       }
+       
+       
+   };
+   
+   return obj;
+});
