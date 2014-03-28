aaebfbd90dd9e5a8f938b1166c3535d1b0f526f0
mark <mark@ca16bcf9-9eb2-46e4-97b8-9b07c30c95dc>
2014-03-28 15:15:49 +0000

    Update to release versions of default branding and common content in preparation for release

--- forgerock-doc-maven-plugin/trunk/src/main/java/org/forgerock/doc/maven/AbstractBuildMojo.java
+++ forgerock-doc-maven-plugin/trunk/src/main/java/org/forgerock/doc/maven/AbstractBuildMojo.java
@@ -337,7 +337,7 @@ abstract class AbstractBuildMojo extends AbstractMojo {
     /**
      * Version of the branding artifact to use.
      *
-     * @parameter default-value="2.1.0-SNAPSHOT" property="brandingVersion"
+     * @parameter default-value="2.1.0" property="brandingVersion"
      * @required
      */
     private String brandingVersion;
