1e27c729984cc87ab5d81d7812ea87ce19f24c3a
jenkins <jenkins@ca16bcf9-9eb2-46e4-97b8-9b07c30c95dc>
2014-05-29 20:33:05 +0000

    [maven-release-plugin]  copy for tag 1.0.15

---
+++ forgerock-ui/tags/1.0.15/forgerock-ui-user/src/main/js/config/messages/UserMessages.js
@@ -0,0 +1,101 @@
+/** 
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright (c) 2011-2012 ForgeRock AS. All rights reserved.
+ *
+ * The contents of this file are subject to the terms
+ * of the Common Development and Distribution License
+ * (the License). You may not use this file except in
+ * compliance with the License.
+ *
+ * You can obtain a copy of the License at
+ * http://forgerock.org/license/CDDLv1.0.html
+ * See the License for the specific language governing
+ * permission and limitations under the License.
+ *
+ * When distributing Covered Code, include this CDDL
+ * Header Notice in each file and include the License file
+ * at http://forgerock.org/license/CDDLv1.0.html
+ * If applicable, add the following below the CDDL Header,
+ * with the fields enclosed by brackets [] replaced by
+ * your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ */
+
+/*global define*/
+
+/**
+ * @author jdabrowski
+ */
+define("config/messages/UserMessages", [
+], function() {
+    
+    var obj = {
+            "usernameNotFound": {
+                msg: "config.messages.UserMessages.usernameNotFound",
+                type: "error"
+            },
+            "noEmailProvided": {
+                msg: "config.messages.UserMessages.noEmailProvided",
+                type: "error"
+            },
+            "emailNotSent": {
+                msg: "config.messages.UserMessages.emailNotSent",
+                type: "error"
+            },
+            "unableToRegister": {
+                msg: "config.messages.UserMessages.unableToRegister",
+                type: "error"
+            },
+            "invalidOldPassword": {
+                msg: "config.messages.UserMessages.invalidOldPassword",
+                type: "error"
+            },
+            "changedPassword": {
+                msg: "config.messages.UserMessages.changedPassword",
+                type: "info"
+            },
+            "profileUpdateFailed": {
+                msg: "config.messages.UserMessages.profileUpdateFailed",
+                type: "error"
+            },
+            "profileUpdateSuccessful": {
+                msg: "config.messages.UserMessages.profileUpdateSuccessful",
+                type: "info"
+            },
+            "userNameUpdated": {
+                msg: "config.messages.UserMessages.userNameUpdated",
+                type: "info"
+            },
+            "afterRegistration": {
+                msg: "config.messages.UserMessages.afterRegistration",
+                type: "info"
+            },
+            "siteIdentificationChanged": {
+                msg: "config.messages.UserMessages.siteIdentificationChanged",
+                type: "info"
+            },
+            "securityDataChanged": {
+                msg: "config.messages.UserMessages.securityDataChanged",
+                type: "info"
+            },
+            "userAlreadyExists": {
+                msg: "config.messages.UserMessages.userAlreadyExists",
+                type: "error"
+            },
+            "errorDeletingNotification": {
+                msg: "config.messages.UserMessages.errorDeletingNotification",
+                type: "error"
+            },
+            "errorFetchingNotifications": {
+                msg: "config.messages.UserMessages.errorFetchingNotifications",
+                type: "error"
+            },
+            "identityNoSpace": {
+                msg: "config.messages.UserMessages.identityNoSpace",
+                type: "error"
+            }
+    };
+    
+    return obj;
+});
---
+++ forgerock-ui/tags/1.0.15/forgerock-ui-user/src/test/qunit/user.js
@@ -0,0 +1,137 @@
+/**
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright (c) 2014 ForgeRock AS. All Rights Reserved
+ *
+ * The contents of this file are subject to the terms
+ * of the Common Development and Distribution License
+ * (the License). You may not use this file except in
+ * compliance with the License.
+ *
+ * You can obtain a copy of the License at
+ * http://forgerock.org/license/CDDLv1.0.html
+ * See the License for the specific language governing
+ * permission and limitations under the License.
+ *
+ * When distributing Covered Code, include this CDDL
+ * Header Notice in each file and include the License file
+ * at http://forgerock.org/license/CDDLv1.0.html
+ * If applicable, add the following below the CDDL Header,
+ * with the fields enclosed by brackets [] replaced by
+ * your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ */
+
+/*global require, define, QUnit */
+
+define([
+        "org/forgerock/commons/ui/common/main/Configuration"
+    ], function (conf) {
+    return {
+        executeAll: function (server) {
+
+            var testPromise = $.Deferred();
+
+            
+            module('User Tests');
+
+            QUnit.test("Logged in user", function () {
+
+                QUnit.ok(conf.loggedUser !== null && conf.loggedUser !== undefined, "There must be a user logged in before running this test suite");
+
+            });
+
+            //Test 1: Update User Info
+ 
+            QUnit.asyncTest("Update User Info", function () {
+
+                var userProfileView = require("UserProfileView");
+                userProfileView.element = $("<div>")[0];
+
+                delete userProfileView.route; // necessary to prevent some error-checking code from causing problems in this context
+
+                userProfileView.render(null,function() {
+
+                    var testVals = {
+                            uid                 : 'Username',
+                            givenName           : 'John',
+                            mail                : 'test@test.com',
+                            sn                  : 'Doe',
+                            telephoneNumber     : '123456789'
+                        },
+                        modifiedUser = _.extend(_.clone(conf.loggedUser), testVals);
+
+
+                    QUnit.start();
+
+                    // Testing inputs
+                    QUnit.ok($('input[name="saveButton"]', userProfileView.$el).length          , "Update button appears to be defined");
+                    QUnit.ok($('input[name="resetButton"]', userProfileView.$el).length         , "Reset button appears to be defined");
+                    QUnit.ok($('input[name="uid"]', userProfileView.$el).length                 , "Username input appears to be defined");
+                    QUnit.ok($('input[name="givenName"]', userProfileView.$el).length           , "First name input appears to be defined");
+                    QUnit.ok($('input[name="mail"]', userProfileView.$el).length                , "Email address input appears to be defined");
+                    QUnit.ok($('input[name="sn"]', userProfileView.$el).length                  , "Last Name input appears to be defined");
+                    QUnit.ok($('input[name="telephoneNumber"]', userProfileView.$el).length     , "Mobile Phone input appears to be defined");
+
+
+                   // Testing user data 
+                    QUnit.equal($('input[name="uid"]', userProfileView.$el).val(), conf.loggedUser.uid                          , "Username populated");
+                    QUnit.equal($('input[name="givenName"]', userProfileView.$el).val(), conf.loggedUser.givenName              , "First name populated");
+                    QUnit.equal($('input[name="mail"]', userProfileView.$el).val(), conf.loggedUser.mail                        , "Email address populated");
+                    QUnit.equal($('input[name="sn"]', userProfileView.$el).val(), conf.loggedUser.sn                            , "Last Name populated");
+                    QUnit.equal($('input[name="telephoneNumber"]', userProfileView.$el).val(), conf.loggedUser.telephoneNumber  , "Mobile Phone populated");
+
+                  
+                    // Testing validation
+                    
+                    QUnit.equal($('input[name="givenName"]', userProfileView.$el).attr('data-validation-status'),          'ok', 'First name input passes validation');
+                    QUnit.equal($('input[name="mail"]', userProfileView.$el).attr('data-validation-status'),               'ok', 'Email address input passes validation');
+                    QUnit.equal($('input[name="sn"]', userProfileView.$el).attr('data-validation-status'),                 'ok', 'Last Name input passes validation');
+                    QUnit.equal($('input[name="telephoneNumber"]', userProfileView.$el).attr('data-validation-status'),    'ok', 'Mobile Phone input passes validation');
+
+                    //Testing Buttons
+
+                    $('input[name="uid"]', userProfileView.$el).val(testVals.uid).trigger('change');
+                    $('input[name="givenName"]', userProfileView.$el).val(testVals.givenName).trigger('change');
+                    $('input[name="mail"]', userProfileView.$el).val(testVals.mail).trigger('change');
+                    $('input[name="sn"]', userProfileView.$el).val(testVals.sn).trigger('change');
+                    $('input[name="telephoneNumber"]', userProfileView.$el).val(testVals.telephoneNumber).trigger('change');
+
+                    //clicking submit button
+                    
+                    $('input[name="saveButton"]', userProfileView.$el).trigger('click');
+
+                    QUnit.equal(conf.loggedUser.uid, testVals.uid                             , "Username changed");
+                    QUnit.equal(conf.loggedUser.givenName, testVals.givenName                 , "First name changed");
+                    QUnit.equal(conf.loggedUser.mail, testVals.mail                           , "Email changed");
+                    QUnit.equal(conf.loggedUser.sn, testVals.sn                               , "Last Name changed");
+                    QUnit.equal(conf.loggedUser.telephoneNumber, testVals.telephoneNumber     , "Mobile Phone changed");
+
+                    QUnit.ok(_.isEqual(conf.loggedUser, modifiedUser)                         , "User object doesn't have any unexpected changes");
+
+                    //reset button
+
+                    $('input[name="uid"]', userProfileView.$el).val('AnotherUsername').trigger('change');
+                    $('input[name="givenName"]', userProfileView.$el).val('Jane').trigger('change');
+                    $('input[name="mail"]', userProfileView.$el).val('test2@test.com').trigger('change');
+                    $('input[name="sn"]', userProfileView.$el).val('Doe').trigger('change');
+                    $('input[name="telephoneNumber"]', userProfileView.$el).val('987654321').trigger('change');
+
+                    //clicking reset button
+                    $('input[name="resetButton"]', userProfileView.$el).trigger('click'); 
+
+                    QUnit.equal($('input[name="uid"]', userProfileView.$el).val(), testVals.uid                             , "Username was reset");
+                    QUnit.equal($('input[name="givenName"]', userProfileView.$el).val(), testVals.givenName                 , "First name was reset");
+                    QUnit.equal($('input[name="mail"]', userProfileView.$el).val(), testVals.mail                           , "Email was reset");
+                    QUnit.equal($('input[name="sn"]', userProfileView.$el).val(), testVals.sn                               , "Last Name was reset");
+                    QUnit.equal($('input[name="telephoneNumber"]', userProfileView.$el).val(), testVals.telephoneNumber     , "Mobile Phone was reset");
+
+                   
+                    testPromise.resolve(); // make sure this is only called after the last async test is finished
+                });
+            });
+
+            return testPromise;
+        }
+    };
+});
