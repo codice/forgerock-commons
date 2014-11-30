27fc311c7972b7358d2c4e29f4416f9ddbbdb562
violette <violette@ca16bcf9-9eb2-46e4-97b8-9b07c30c95dc>
2014-11-30 12:43:03 +0000

    Make checkstyle happy

--- mobile/contact-manager/trunk/src/org/forgerock/contactmanager/SearchActivity.java
+++ mobile/contact-manager/trunk/src/org/forgerock/contactmanager/SearchActivity.java
@@ -31,10 +31,19 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.text.Editable;
-import android.view.*;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 import android.view.View.OnKeyListener;
-import android.widget.*;
+import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.ProgressBar;
 
 /**
  * This class is main activity of this application.
