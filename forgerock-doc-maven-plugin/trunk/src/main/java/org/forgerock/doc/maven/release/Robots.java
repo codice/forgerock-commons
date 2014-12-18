3a8ed2db977b529bb862c8436a9cd1dfc0dd0843
mark <mark@ca16bcf9-9eb2-46e4-97b8-9b07c30c95dc>
2014-12-18 15:04:03 +0000

    CR-5734 DOCS-246 Move to CDDL
    This patch moves forgerock-doc-maven-plugin
    to use the same license as other FR software.

--- forgerock-doc-maven-plugin/trunk/src/main/java/org/forgerock/doc/maven/release/Robots.java
+++ forgerock-doc-maven-plugin/trunk/src/main/java/org/forgerock/doc/maven/release/Robots.java
@@ -1,15 +1,17 @@
 /*
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * The contents of this file are subject to the terms of the Common Development and
+ * Distribution License (the License). You may not use this file except in compliance with the
+ * License.
  *
- * If applicable, add the following below this MPL 2.0 HEADER, replacing
- * the fields enclosed by brackets "[]" replaced with your own identifying
- * information:
- *     Portions Copyright [yyyy] [name of copyright owner]
+ * You can obtain a copy of the License at legal/CDDLv1.0.txt. See the License for the
+ * specific language governing permission and limitations under the License.
  *
- *     Copyright 2014 ForgeRock AS
+ * When distributing Covered Software, include this CDDL Header Notice in each file and include
+ * the License file at legal/CDDLv1.0.txt. If applicable, add the following below the CDDL
+ * Header, with the fields enclosed by brackets [] replaced by your own identifying
+ * information: "Portions copyright [year] [name of copyright owner]".
  *
+ * Copyright 2014 ForgeRock AS
  */
 
 package org.forgerock.doc.maven.release;
