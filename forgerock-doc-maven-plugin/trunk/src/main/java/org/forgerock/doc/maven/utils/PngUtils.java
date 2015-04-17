865a94f081dd2c2ac4e5dde69423b57b87cf5516
chris.lee <chris.lee@ca16bcf9-9eb2-46e4-97b8-9b07c30c95dc>
2015-04-17 16:28:41 +0000

    DOCS-282 CR-6690 Use multi-pass method to resize images for thumbnails.

--- forgerock-doc-maven-plugin/trunk/src/main/java/org/forgerock/doc/maven/utils/PngUtils.java
+++ forgerock-doc-maven-plugin/trunk/src/main/java/org/forgerock/doc/maven/utils/PngUtils.java
@@ -29,7 +29,7 @@ import javax.imageio.metadata.IIOMetadataNode;
 import javax.imageio.stream.ImageOutputStream;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
-import java.awt.AlphaComposite;
+import java.awt.Transparency;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -74,7 +74,7 @@ public final class PngUtils {
 
         final int imageWidth = getWidth(image);
         final int imageHeight = getHeight(image);
-        final int newWidth = 720;
+        final int newWidth = 700;
 
         String absolutePath = image.getAbsolutePath();
 
@@ -89,33 +89,91 @@ public final class PngUtils {
             final int newHeight = Math.round(imageHeight * newWidth / imageWidth);
 
             System.out.println("Creating thumbnail of: " + image.getName()
-                + " (" + newWidth + " x " + newHeight + ")");
-            int imageType = BufferedImage.TYPE_INT_ARGB;
-            BufferedImage scaledBI = new BufferedImage(newWidth, newHeight, imageType);
-            Graphics2D g = scaledBI.createGraphics();
-            g.setRenderingHint(RenderingHints.KEY_RENDERING,
-                    RenderingHints.VALUE_RENDER_QUALITY);
-            g.setRenderingHint(RenderingHints.KEY_COLOR_RENDERING,
-                    RenderingHints.VALUE_COLOR_RENDER_QUALITY);
-            g.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
-                    RenderingHints.VALUE_ANTIALIAS_ON);
-            g.setRenderingHint(RenderingHints.KEY_INTERPOLATION,
-                    RenderingHints.VALUE_INTERPOLATION_BILINEAR);
-            g.setRenderingHint(RenderingHints.KEY_ALPHA_INTERPOLATION,
-                    RenderingHints.VALUE_ALPHA_INTERPOLATION_QUALITY);
-            g.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,
-                    RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
-            g.setRenderingHint(RenderingHints.KEY_DITHERING,
-                    RenderingHints.VALUE_DITHER_ENABLE);
+                    + " (" + newWidth + " x " + newHeight + ")");
 
-            g.setComposite(AlphaComposite.Src);
-            g.drawImage(originalImage, 0, 0, newWidth, newHeight, null);
-            g.dispose();
+            BufferedImage scaledBI = getScaledInstance(
+                    originalImage,
+                    newWidth,
+                    newHeight,
+                    RenderingHints.VALUE_INTERPOLATION_BILINEAR,
+                    true);
 
             saveBufferedImage(scaledBI, thumbFile, 160);
         } else {
             saveBufferedImage(originalImage, thumbFile, 160);
         }
+    }
+
+    /**
+     * Convenience method that returns a scaled instance of the
+     * provided {@code BufferedImage}.
+     *
+     * @param img the original image to be scaled
+     * @param targetWidth the desired width of the scaled instance,
+     *    in pixels
+     * @param targetHeight the desired height of the scaled instance,
+     *    in pixels
+     * @param hint one of the rendering hints that corresponds to
+     *    {@code RenderingHints.KEY_INTERPOLATION} (e.g.
+     *    {@code RenderingHints.VALUE_INTERPOLATION_NEAREST_NEIGHBOR},
+     *    {@code RenderingHints.VALUE_INTERPOLATION_BILINEAR},
+     *    {@code RenderingHints.VALUE_INTERPOLATION_BICUBIC})
+     * @param higherQuality if true, this method will use a multi-step
+     *    scaling technique that provides higher quality than the usual
+     *    one-step technique (only useful in downscaling cases, where
+     *    {@code targetWidth} or {@code targetHeight} is
+     *    smaller than the original dimensions, and generally only when
+     *    the {@code BILINEAR} hint is specified)
+     * @return a scaled version of the original {@code BufferedImage}
+     */
+    public static BufferedImage getScaledInstance(
+        BufferedImage img,
+        int targetWidth,
+        int targetHeight,
+        Object hint,
+        boolean higherQuality) {
+        int type = (img.getTransparency() == Transparency.OPAQUE)
+                ? BufferedImage.TYPE_INT_RGB : BufferedImage.TYPE_INT_ARGB;
+        BufferedImage ret = (BufferedImage) img;
+        int w, h;
+        if (higherQuality) {
+            // Use multi-step technique: start with original size, then
+            // scale down in multiple passes with drawImage()
+            // until the target size is reached
+            w = img.getWidth();
+            h = img.getHeight();
+        } else {
+            // Use one-step technique: scale directly from original
+            // size to target size with a single drawImage() call
+            w = targetWidth;
+            h = targetHeight;
+        }
+
+        do {
+            if (higherQuality && w > targetWidth) {
+                w /= 2;
+                if (w < targetWidth) {
+                    w = targetWidth;
+                }
+            }
+
+            if (higherQuality && h > targetHeight) {
+                h /= 2;
+                if (h < targetHeight) {
+                    h = targetHeight;
+                }
+            }
+
+            BufferedImage tmp = new BufferedImage(w, h, type);
+            Graphics2D g2 = tmp.createGraphics();
+            g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, hint);
+            g2.drawImage(ret, 0, 0, w, h, null);
+            g2.dispose();
+
+            ret = tmp;
+        } while (w != targetWidth || h != targetHeight);
+
+        return ret;
     }
 
     /**
