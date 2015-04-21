aa240e0c4fe575f438034d015b4f8fe2f77cf70c
jake.feasel <jake.feasel@ca16bcf9-9eb2-46e4-97b8-9b07c30c95dc>
2015-04-21 03:59:48 +0000

    [maven-release-plugin]  copy for branch 2.0.3

---
+++ forgerock-ui/branches/2.0.3/forgerock-ui-user/src/main/js/config/routes/UserRoutesConfig.js
@@ -0,0 +1,60 @@
+/** 
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright (c) 2011-2014 ForgeRock AS. All rights reserved.
+ *
+ * The contents of this file are subject to the terms
+ * of the Common Development and Distribution License
+ * (the License). You may not use this file except in
+ * compliance with the License.
+ *
+ * You can obtain a copy of the License at
+ * http://forgerock.org/license/CDDLv1.0.html
+ * See the License for the specific language governing
+ * permission and limitations under the License.
+ *
+ * When distributing Covered Code, include this CDDL
+ * Header Notice in each file and include the License file
+ * at http://forgerock.org/license/CDDLv1.0.html
+ * If applicable, add the following below the CDDL Header,
+ * with the fields enclosed by brackets [] replaced by
+ * your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ */
+
+/*global define*/
+/*jslint regexp:false */
+
+/**
+ * @author jdabrowski
+ */
+define("config/routes/UserRoutesConfig", [
+], function() {
+    //definitions for views here are generic
+    //the actual path to each view is defined in config/AppConfiguration.js
+    //view files are loaded when the GenericRouteInterfaceMap module is initialized
+    var obj = {
+        "profile": {
+            view: "UserProfileView",
+            role: "ui-user",
+            url: "profile/" ,
+            forceUpdate: true
+        },
+        "selfRegistration": {
+            view: "RegisterView",
+            url: /register(\/[^\&]*)(\&.+)?/,
+            pattern: "register??",
+            forceUpdate: true,
+            argumentNames: ["realm", "additionalParameters"],
+            defaults: ["/",""]
+        },
+        "changeSecurityData": {
+            base: "profile",
+            dialog: "ChangeSecurityDataDialog",
+            role: "ui-user,ui-admin",
+            url: "profile/change_security_data/"
+        }
+    };
+    
+    return obj;
+});
